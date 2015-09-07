f9c041a24ab262c37df97b7ceeeb800ba884eb36
brianbier <briansbier@gmail.com>
2015-09-06 23:20:40 -0400

    test complete

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+  num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(phrase)
+  phrase = "I'm right and you are wrong!"
+
 end
 
-def greeting
+def greeting(argue1,argue2)
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+return topping
 end
+pizza_party("pepperoni")
